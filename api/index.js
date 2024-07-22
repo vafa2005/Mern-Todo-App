c5ebc50e7bf245194df341aeb49e45ab9f18d94d
rahmath <rahmathck381@gmail.com>
2024-07-22 14:28:44 +0530

    Express installed, Server created

--- api/index.js
+++ api/index.js
@@ -1,2 +1,8 @@
-console.log('Hello');
-console.log('world!');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
