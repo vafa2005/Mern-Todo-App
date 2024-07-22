6af1a8ace280918beb71c2ec18fae5894de18928
Abusinan <abuk9820@gmail.com>
2024-07-22 14:55:08 +0530

    Routes created

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,8 +2,20 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+app.get('/get-all-todo', (req, res) => {
+    // To get all todos
+});
+
+app.post('/create-new-todo', (req, res) => {
+    // To create new todo
+});
+
+app.patch('/', (req, res) => {
+    // To edit a todo
+});
+
+app.delete('/', (req, res) => {
+    //To delete a todo
+});
 
 export default app;
