db7ff87e3cb33bf3dec1558426acc030d7fc6ff9
Abusinan <abuk9820@gmail.com>
2024-07-22 14:44:00 +0530

    route created

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import userRoute from "./routes/user.route.js"
 
 const app = express();
 const port = 3000;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-})
+});
+
+app.use('/user', userRoute);
